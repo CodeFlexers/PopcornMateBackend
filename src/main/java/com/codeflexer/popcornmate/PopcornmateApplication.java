3f075beaefdb1c91b65b925e0f8f2f685ced7df6
Seopia <wjdwltjq7289@gmail.com>
2025-05-17 18:53:01 +0900

    first

---
+++ src/main/java/com/codeflexer/popcornmate/PopcornmateApplication.java
@@ -0,0 +1,13 @@
+package com.codeflexer.popcornmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornmateApplication.class, args);
+	}
+
+}
